e1af470c23728467bff9ec5195977ad039989340
spec <scotmcampbell@icloud.com>
2023-07-08 00:30:07 -0500

    add organization to module loading

--- loader.lua
+++ loader.lua
@@ -21,11 +21,12 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/scotdotwtf/Final16/ma
 --// vars and funcs
 local repo = "https://raw.githubusercontent.com/scotdotwtf/Final16/main/"
 if modules == nil then
-    getgenv().modules = {"camera.lua", "displays.lua", "graphics.lua", "kick.lua", "fd_spoof.lua", "ui_contents.lua"}
+    --//   organizaton: | graphics                       | control     | ui                           | developer
+    getgenv().modules = {"displays.lua", "graphics.lua",  "camera.lua", "kick.lua", "ui_contents.lua", "fd_spoof.lua"}
     game:GetService("TestService"):Warn(false, "[!] final16 | modules nil")
 end
 
-for number, module in pairs(modules) do
+for number, module in pairs(modules) do 
     loadstring(game:HttpGet(repo.."modules/"..module))()
     game:GetService("TestService"):Message("[*] final16 | loaded module, "..module)
- end
+end

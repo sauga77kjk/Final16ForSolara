2f6f037e401a2e29446acd5b8ee5ec4ca001156a
sauga77kjk <169109325+sauga77kjk@users.noreply.github.com>
2024-07-07 18:57:42 +0900

    Update fd_spoof.lua

--- modules/fd_spoof.lua
+++ modules/fd_spoof.lua
@@ -1,4 +1,7 @@
 --// metamethod FD spoof (not mine, credits to who made it lol)
+local function sethiddenproperty(obj,pro,val)
+obj[pro]=val
+end
 local Workspace = game.Workspace
 local old
 old = hookmetamethod(game, "__index", newcclosure(function(Self, ...)

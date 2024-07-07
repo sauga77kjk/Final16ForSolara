09dcd78f35242dd53b9a68f0827f99498718328f
sauga77kjk <169109325+sauga77kjk@users.noreply.github.com>
2024-07-07 18:58:06 +0900

    Update kick.lua

--- modules/kick.lua
+++ modules/kick.lua
@@ -1,4 +1,7 @@
 --// remove new kick
+local function sethiddenproperty(obj,pro,val)
+obj[pro]=val
+end
 game:GetService("RunService").RenderStepped:Connect(function()
     game:GetService("RunService"):SetRobloxGuiFocused(false)
     for _, v in pairs(game:GetService("CoreGui"):GetChildren()) do
